6c3770f24b54ee5f8ce286e92b226d585d0a6133
rothmonychhem <2243114@cmontmorency.qc.ca>
2024-11-08 00:13:30 -0500

    better

--- server/server.js
+++ server/server.js
@@ -1,60 +1,49 @@
-import express from 'express'; // Changer require en import
+import express from 'express';
 import dotenv from 'dotenv'; 
 import offreRoute from './routes/offreEmploi.js';
 import CandidatRoute from './routes/candidat.js';
 import EntrepriseRoute from './routes/entreprise.js';
 import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import path from'path';
-import {fileURLToPath} from "url";
-
-const __filename= fileURLToPath(import.meta.url)
-const __dirname = path.__dirname(__filename)
-
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
 
-
-// Middleware pour gérer les requêtes en JSON
-app.use(express.json())
-app.use((req,res,next)=>{
-  console.log(req.path, req.method)
-  next()
+// Middleware for JSON requests
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
-// Route 
-app.use('/api/offreEmploi/',offreRoute);
-app.use('/api/candidat/',CandidatRoute);
-app.use('/api/entreprise/',EntrepriseRoute);
+// Routes
+app.use('/api/offreEmploi/', offreRoute);
+app.use('/api/candidat/', CandidatRoute);
+app.use('/api/entreprise/', EntrepriseRoute);
 
-app.use(express.static(path.join(__dirname, '/client/build')));
-
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/client/build/index.html'))
 );
 
-
-//connexion mongoDB
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
-// Démarrer le serveur
-app.listen(process.env.PORT, () => {
-  console.log(`Connecter a mongo db et Backend is running on http://localhost:${process.env.PORT}`);
-});
-})
-.catch((error)=>{
-  console.log(error)
-});
+  .then(() => {
+    // Start the server
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to MongoDB and backend is running on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-
-//app.get()
+// Test route
 app.get('/', (req, res) => {
-    res.json({mggs: 'Hi .... OHHH!!'}); // Exemple de route
+  res.json({ msg: 'Hi .... OHHH!!' });
 });
-
-
-
-
